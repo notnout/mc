ae02ad885c93ac70a8aab37018ac5811788a6485
Yury V. Zaytsev <yury@shurup.com>
2024-06-03 16:12:32 +0200

    utilvfs: move functions from h-file into c-module to avoid config.h pollution
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- lib/vfs/utilvfs.h
+++ lib/vfs/utilvfs.h
@@ -9,12 +9,6 @@
 #ifndef MC_VFS_UTILVFS_H
 #define MC_VFS_UTILVFS_H
 
-#include <config.h>             /* HAVE_UTIMENSAT, HAVE_UTIME_H */
-
-#if !defined (HAVE_UTIMENSAT) && defined (HAVE_UTIME_H)
-#include <utime.h>
-#endif
-
 #include <sys/stat.h>
 
 #include "lib/global.h"
@@ -66,94 +60,13 @@ size_t vfs_parse_ls_lga_get_final_spaces (void);
 gboolean vfs_parse_month (const char *str, struct tm *tim);
 int vfs_parse_filedate (int idx, time_t * t);
 
+int vfs_utime (const char *path, mc_timesbuf_t * times);
+void vfs_get_timespecs_from_timesbuf (mc_timesbuf_t * times, mc_timespec_t * atime,
+                                      mc_timespec_t * mtime);
+void vfs_get_timesbuf_from_stat (const struct stat *s, mc_timesbuf_t * times);
+void vfs_copy_stat_times (const struct stat *src, struct stat *dst);
+void vfs_zero_stat_times (struct stat *s);
+
 /*** inline functions ****************************************************************************/
-
-static inline int
-vfs_utime (const char *path, mc_timesbuf_t *times)
-{
-#ifdef HAVE_UTIMENSAT
-    return utimensat (AT_FDCWD, path, *times, AT_SYMLINK_NOFOLLOW);
-#else
-    return utime (path, times);
-#endif
-}
-
-static inline void
-vfs_get_timespecs_from_timesbuf (mc_timesbuf_t *times, mc_timespec_t *atime, mc_timespec_t *mtime)
-{
-#ifdef HAVE_UTIMENSAT
-    atime->tv_sec = (*times)[0].tv_sec;
-    atime->tv_nsec = (*times)[0].tv_nsec;
-    mtime->tv_sec = (*times)[1].tv_sec;
-    mtime->tv_nsec = (*times)[1].tv_nsec;
-#else
-    atime->tv_sec = times->actime;
-    atime->tv_nsec = 0;
-    mtime->tv_sec = times->modtime;
-    mtime->tv_nsec = 0;
-#endif
-}
-
-static inline void
-vfs_get_timesbuf_from_stat (const struct stat *s, mc_timesbuf_t *times)
-{
-#ifdef HAVE_UTIMENSAT
-#ifdef HAVE_STRUCT_STAT_ST_MTIM
-    /* POSIX IEEE Std 1003.1-2008 should be the preferred way
-     *
-     * AIX has internal type st_timespec_t conflicting with timespec, so assign per field, for details see:
-     * https://github.com/libuv/libuv/pull/4404
-     */
-    (*times)[0].tv_sec = s->st_atim.tv_sec;
-    (*times)[0].tv_nsec = s->st_atim.tv_nsec;
-    (*times)[1].tv_sec = s->st_mtim.tv_sec;
-    (*times)[1].tv_nsec = s->st_mtim.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIMESPEC
-    /* Modern BSD solution */
-    (*times)[0] = s->st_atimespec;
-    (*times)[1] = s->st_mtimespec;
-#elif HAVE_STRUCT_STAT_ST_MTIMENSEC
-    /* Legacy BSD solution */
-    (*times)[0].tv_sec = s->st_atime;
-    (*times)[0].tv_nsec = s->st_atimensec;
-    (*times)[1].tv_sec = s->st_mtime;
-    (*times)[1].tv_nsec = s->st_mtimensec;
-#else
-#error "Found utimensat for nanosecond timestamps, but unsupported struct stat format!"
-#endif
-#else
-    times->actime = s->st_atime;
-    times->modtime = s->st_mtime;
-#endif
-}
-
-static inline void
-vfs_copy_stat_times (const struct stat *src, struct stat *dst)
-{
-    dst->st_atime = src->st_atime;
-    dst->st_mtime = src->st_mtime;
-    dst->st_ctime = src->st_ctime;
-
-#ifdef HAVE_STRUCT_STAT_ST_MTIM
-    dst->st_atim.tv_nsec = src->st_atim.tv_nsec;
-    dst->st_mtim.tv_nsec = src->st_mtim.tv_nsec;
-    dst->st_ctim.tv_nsec = src->st_ctim.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIMESPEC
-    dst->st_atimespec.tv_nsec = src->st_atimespec.tv_nsec;
-    dst->st_mtimespec.tv_nsec = src->st_mtimespec.tv_nsec;
-    dst->st_ctimespec.tv_nsec = src->st_ctimespec.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIMENSEC
-    dst->st_atimensec = src->st_atimensec;
-    dst->st_mtimensec = src->st_atimensec;
-    dst->st_ctimensec = src->st_atimensec;
-#endif
-}
-
-static inline void
-vfs_zero_stat_times (struct stat *s)
-{
-    const struct stat empty = { 0 };
-    vfs_copy_stat_times (&empty, s);
-}
 
 #endif
